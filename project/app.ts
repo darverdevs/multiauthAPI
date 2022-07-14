6d4de7f66d235a4bca4b5c4c4cfd54fcb2f8b76b
primboi <81822069+primitt@users.noreply.github.com>
2022-07-14 09:17:36 -0700

    Initial Commit

---
+++ project/app.ts
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+console.log("started")
+app.get('/', (req: any, res: any) => {
+    res.send("MultiAuth Server");
+});
+app.post('/login', (req: any, res: any) => {
+    var headers = req.headers;
+    if (headers.UID == undefined){
+        res.send("UID not found");
+    }
+    else{
+        res.send("Username: " + headers.username);
+    }
+})
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
