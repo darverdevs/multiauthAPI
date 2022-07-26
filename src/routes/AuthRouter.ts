859912f25c90e9a18d33d851fd105ae76c01ec2d
primboi <81822069+primitt@users.noreply.github.com>
2022-07-26 09:37:16 -0700

    Add session code + blocklist

--- src/routes/AuthRouter.ts
+++ src/routes/AuthRouter.ts
@@ -2,10 +2,9 @@ import { Router, Request, Response } from "express";
 import argon2, { argon2id } from "argon2";
 import crypto from "crypto";
 import prisma from "../db";
-
+import blocklist from "../blocklist";
 
 const router = Router();
-
 router.get("/", (_req: Request, res: Response) => res.json({
     success: true,
     message: "Auth Endpoint.",
@@ -22,46 +21,85 @@ router.post("/login", async (req: Request, res: Response) => {
             success: false,
             message: "No request body specified.",
         });
-    const { username, password } = req.body;
+    const { username, password, session } = req.body;
      // Defines variables username and password from req.body
     console.log(username, password);
-    if (!username || !password) // If the username or password field(s) are not specified in the request body, return response with status 400 (Bad Request)
-        return res.status(400).json({
-            success: false,
-            message: "One or more required fields were missing from the request body.",
+    if (!session){
+        if (!username || !password) // If the username or password field(s) are not specified in the request body, return response with status 400 (Bad Request)
+            return res.status(400).json({
+                success: false,
+                message: "One or more required fields were missing from the request body.",
+            });      
+        const user = await prisma.user.findUnique({
+            where: {
+                username,
+            },
         });
 
-    const user = await prisma.user.findUnique({
-        where: {
-            username,
-        },
-    });
-
-    if (!user) // If a user by that username does not exist, return response with status 400 (Bad Request)
-        return res.status(400).json({
-            success: false,
-            message: "Username or password is invalid.",
-        });
-    
-    const passwordMatches = await argon2.verify(user.password, password, {
-        type: argon2id,
-    });
-
-    if (!passwordMatches)
-        return res.status(400).json({
-            success: false,
-            message: "Username or password is invalid.",
+        if (!user) // If a user by that username does not exist, return response with status 400 (Bad Request)
+            return res.status(400).json({
+                success: false,
+                message: "Username or password is invalid.",
+            });
+        
+        const passwordMatches = await argon2.verify(user.password, password, {
+            type: argon2id,
         });
 
-    
-    return res.json({
-        success: true,
-        message: "Successfully logged in.",
-        data: { // For easier rendering on frontend. One less request!
-            uuid: user.uuid,
-            username: user.username,
-        },
-    });
+        if (!passwordMatches)
+            return res.status(400).json({
+                success: false,
+                message: "Username or password is invalid.",
+            });
+
+        // TODO - Create Session Cookie
+        return res.json({
+            success: true,
+            message: "Successfully logged in.",
+            data: { // For easier rendering on frontend. One less request!
+                uuid: user.uuid,
+                username: user.username,
+            },
+        });
+    }
+    else{
+        const sessionId = await prisma.session.findUnique({
+            where: {
+                token: session,
+            }
+        });
+        if (!sessionId)
+        {
+            return res.status(400).json({
+                success: false,
+                message: "Session is invalid.",
+            });
+        }
+        else{
+            const user = await prisma.user.findUnique({
+                where: {
+                    uuid: sessionId.uuid,
+                }
+            });
+            if (!user)
+            {
+                return res.status(400).json({
+                    success: false,
+                    message: "User is invalid.",
+                });
+            }
+            else{
+                return res.json({
+                    success: true,
+                    message: "Successfully logged in.",
+                    data: { // For easier rendering on frontend. One less request!
+                        uuid: user.uuid,
+                        username: user.username,
+                    },
+                });
+            }
+        }
+    }
 });
 
 router.post("/register", async (req: Request, res: Response) => {
@@ -73,6 +111,15 @@ router.post("/register", async (req: Request, res: Response) => {
             message: "One or more required fields were missing from the request body.",
         });
     }
+    // Check if username has blocked words
+    for (let i=0; i<blocklist.length; i++){
+        if (username.includes(blocklist[i])){
+            return res.status(400).json({
+                success: false,
+                message: "Username contains a blocked word.",
+            });
+        }
+    } 
     const user = await prisma.user.findUnique({
         where: {
             username,
@@ -118,4 +165,32 @@ router.post("/register", async (req: Request, res: Response) => {
         });
     }
 });
+router.post("/isreg", async (req: Request, res: Response) => {
+    const { username } = req.body;
+    if (!username){
+        return res.status(400).json({
+            success: false,
+            message: "One or more required fields were missing from the request body.",
+        });
+    }
+    const user = await prisma.user.findUnique({
+        where: {
+            username,
+        },
+    });
+    if (!user){
+        return res.json({
+            success: true,
+            isreg: false,
+            message: "Username is not registered.",
+        });
+    }
+    else{
+        return res.json({
+            success: true,
+            isreg: true,
+            message: "Username is already registered.",
+        });
+    }
+}); //TODO - Add sessions
 export default router;
